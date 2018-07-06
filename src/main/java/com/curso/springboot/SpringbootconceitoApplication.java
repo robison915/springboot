0fe38dce68048f9c300ca2c650dbdddff58942fc
Robison <robison.nunes01@gmail.com>
2018-07-05 22:45:30 -0300

    Projeto criado

---
+++ src/main/java/com/curso/springboot/SpringbootconceitoApplication.java
@@ -0,0 +1,12 @@
+package com.curso.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootconceitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootconceitoApplication.class, args);
+	}
+}
